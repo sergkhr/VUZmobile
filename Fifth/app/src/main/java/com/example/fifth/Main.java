874ff2e5b88ec6d65dc6757ac475b1f4ce331cfa
sergkhr <serg.khrechko@gmail.com>
2023-04-11 14:34:10 +0300

    half of sixth

--- Fifth/app/src/main/java/com/example/fifth/Main.java
+++ Fifth/app/src/main/java/com/example/fifth/Main.java
@@ -1,9 +1,16 @@
 package com.example.fifth;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -20,33 +27,15 @@ import android.widget.TextView;
  */
 public class Main extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String CHANNEL_ID = "notify";
 
     public Main() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Main.
-     */
     // TODO: Rename and change types and number of parameters
     public static Main newInstance(String param1, String param2) {
         Main fragment = new Main();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,9 +43,18 @@ public class Main extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "notify",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel.setDescription("notify");
+            channel.setLightColor(R.color.purple_200);
+
+            NotificationManager manager = getActivity().getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
         }
     }
 
@@ -101,5 +99,37 @@ public class Main extends Fragment {
                         .navigate(R.id.action_main_to_second, bundle);
             }
         });
+
+        //notify button
+        Button notifyButton = view.findViewById(R.id.notificationBtn);
+        //show notification when clicked
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotification();
+            }
+        });
+    }
+
+
+
+    private void showNotification(){
+        //check for permission
+        if (ContextCompat.checkSelfPermission(getContext(), "android.permission.POST_NOTIFICATIONS") != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{"android.permission.POST_NOTIFICATIONS"}, 1);
+        }
+        else{
+            //permission granted
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Notification")
+                    .setContentText("This is a notification")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getContext());
+
+            notificationManager.notify(200, builder.build());
+        }
+
     }
 }
